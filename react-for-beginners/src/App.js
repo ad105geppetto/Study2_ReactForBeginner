9c25d11dab59ffa4cf796f840f0224fe1329eb8a
geppetto <ad105geppetto@gmail.com>
2022-03-26 05:37:00 +0900

    creat-react-app file  && exercise file

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styled from "./App.module.css"
+
+function App() {
+  return (
+    <div>
+      <h1 className={styled.title}>Welcome back to React</h1>
+      <Button text={'click'}/>
+    </div>
+  );
+}
+
+export default App;
